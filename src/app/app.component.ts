7e2def8377236cfa9569d53089367ff6cff99fbe
Luis Arce <luis.arce22@gmail.com>
2019-04-18 07:35:34 -0400

    Add package lock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'l-construct';
+
+  projects = [
+    {
+      id: 1,
+      name: "project 1"
+    },
+    {
+      id: 2,
+      name: "project 2"
+    },
+  ] 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
+import { NbListModule } from '@nebular/theme';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
     AppRoutingModule,
     BrowserAnimationsModule,
     NbThemeModule.forRoot({ name: 'cosmic' }),
-    NbLayoutModule
+    NbLayoutModule,
+    NbListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
